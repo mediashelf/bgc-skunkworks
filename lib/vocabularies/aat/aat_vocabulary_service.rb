cbb6b115d6dc17e3078c8bc40e1853840edcdcd7
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-21 16:12:53 -0700

    painting in aat_vocabulary_service

--- lib/vocabularies/aat/aat_vocabulary_service.rb
+++ lib/vocabularies/aat/aat_vocabulary_service.rb
@@ -11,6 +11,14 @@ class AATVocabularyService
     @password = GettyVocabularies.instance.password
   end
   
+  def retrieve_subject(subject_id)
+    if subject_id.kind_of?(Hash) 
+      subject_id = BGCIdentityResolver.instance.resolve_id(subject_id[:bgc_id],:aat)
+    end
+    subject_xml = get_subject_xml(subject_id)
+    return AATSubject.from_xml(subject_xml)
+  end
+  
   def get_subject_xml(subject_id)
     aat = open("#{base_uri}/AATGetSubject?subjectID=#{subject_id}", :http_basic_authentication=>[username,password])
     aat.read
--- spec/services/aat_vocabulary_service_spec.rb
+++ spec/services/aat_vocabulary_service_spec.rb
@@ -1,6 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe AATVocabularyService do
+  
+  before(:each) do
+    @subject_xml = fixture("300140808.xml").read
+    AATVocabularyService.instance.stub!(:get_subject_xml).with("300140808").and_return(@subject_xml)
+  end
 
   it "should be kind of VocabularyService" do
     # @service.should be_kind_of VocabularyService
@@ -14,14 +19,15 @@ describe AATVocabularyService do
     end
   end
   
-  describe "retrieve" do
+  describe "retrieve_subject" do
     it "should assume that strings are AAT ids" do
-      aat_subject = AATVocabularyService.instance.retrieve("300140808")
+      aat_subject = AATVocabularyService.instance.retrieve_subject("300140808")
       aat_subject.should be_instance_of(AATSubject)
+      EquivalentXml.equivalent?(aat_subject.ng_xml, @subject_xml)
     end
     it "should use BGCIdentityResolver if :bgc_id is provided" do
-      BGCIdentityResolver.should_receive(:resolve).with("10000002", :aat).and_return("300140808")
-      AATVocabularyService.instance.retrieve(:bgc_id=>"10000002")
+      BGCIdentityResolver.instance.should_receive(:resolve_id).with("10000002", :aat).and_return("300140808")
+      AATVocabularyService.instance.retrieve_subject(:bgc_id=>"10000002")
     end
   end
   
