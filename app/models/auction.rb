79501488c4e4de6985c76cc5f7fbed0895833938
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-25 17:02:44 -0500

    refining auctions

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,3 +1,19 @@
 require "getty_vocabularies"
 class Auction
+  
+  include Blacklight::SolrHelper
+  
+  attr_accessor :solr_doc, :solr_response, :aat, :auction_house, :auction_house_id, :auction_house_sale_url
+  
+  def initialize(new_id=nil)
+    if !new_id.nil?
+      @auction_house_id = BGCIdentityResolver.resolve_id(new_id, :christies)
+      @aat = AATVocabularyService.retrieve_subject(:bgc_id=>new_id)
+      @solr_response, @solr_doc = solr_response_for_doc_id
+    end
+  end
+  
+  def self.load_instance(instance_id=nil)
+    return self.new(instance_id)
+  end
 end
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -3,9 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Auction do
   describe "#load_instance" do
     it "should assemble an Auction object" do
-      SolrService.should_receive(:solr_response_for_doc_id).with("10000002").and_return(["solr response","solr document"])
-      AATVocabularyService.should_receive.expects(:retrieve).with(:bgc_id=>"10000002").and_return("AAT response")
-      BGCIdentityResolver.should_receive(:resolve).with("10000002", :christies).and_return("christies internal id")
+      # SolrService.should_receive(:solr_response_for_doc_id).with("10000002").and_return(["solr response","solr document"])
+      AATVocabularyService.should_receive(:retrieve_subject).with(:bgc_id=>"10000002").and_return("AAT response")
+      BGCIdentityResolver.should_receive(:resolve_id).with("10000002", :christies).and_return("christies internal id")
       auction = Auction.load_instance("10000002")
       auction.aat.should == "AAT response"
       auction.solr_doc.should == "solr document"
