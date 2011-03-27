6e60995c67f219034ec5c1a86eed71139471ebb1
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-27 02:27:46 -0500

    lot images link to auction house lot pages

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -4,6 +4,7 @@ class AuctionsController < ApplicationController
   
   def show
     @auction = Auction.load_instance(params[:id])
+    params[:per_page] ||= "28"
     @lots = @auction.query_for_lots.paginate(:page=>params[:page], :per_page=>params[:per_page])
     
     @response, @document = [@auction.catalog_solr_response, @auction.catalog_solr_doc]
--- app/helpers/auction_helper.rb
+++ app/helpers/auction_helper.rb
@@ -3,7 +3,9 @@ module AuctionHelper
   # Render html for an auction sale lot based on values hash from BGC Auction Lots service
   # @param [Hash] lot_values ex. {"lotNumber":"1861","description":["A LONGQUAN CELADON TRIPOD CENSER",""],"lotURL":"http://www.christies.com/LotFinder/lot_details.aspx?intObjectID=5271823","thumbnail":"http://www.christies.com/lotfinderimages/d52718/d5271823s.jpg"}}
   def render_lot(lot_values)
-    result = image_tag(lot_values["thumbnail"], :alt=>lot_values["description"], :class => "auction_lot")
+    link_content = image_tag(lot_values["thumbnail"], :alt=>lot_values["description"], :class => "auction_lot")
+    link_url = lot_values["lotURL"]
+    result = link_to link_content, link_url
     return result
   end
 end
