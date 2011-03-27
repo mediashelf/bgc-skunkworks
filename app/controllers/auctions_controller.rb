4f7aa4699ace1890f47192527dad8c293ce0dc0c
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-27 00:31:03 -0500

    starter auction show view

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -2,6 +2,7 @@ class AuctionsController < ApplicationController
   
   def show
     @auction = Auction.load_instance(params[:id])
+    @response, @document = [@auction.catalog_solr_response, @auction.catalog_solr_doc]
   end
   
 end
