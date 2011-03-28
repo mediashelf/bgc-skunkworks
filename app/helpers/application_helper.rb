f73ab6759e1b8765eab1d2a02252bd738a14d905
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-28 17:18:40 -0500

    hydra-style/rails-style render_document_partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,5 +20,22 @@ module ApplicationHelper
     end
   end
   
+  # From hydra-head
+  # Overriding Blacklight's render_document_partial
+  # given a doc and action_name, this method attempts to render a partial template
+  # based on the value of doc[:format]
+  # if this value is blank (nil/empty) the "default" is used
+  # if the partial is not found, the "default" partial is rendered instead
+  def render_document_partial(doc, action_name, locals={})
+    format = document_partial_name(doc)
+    begin
+      Rails.logger.debug("attempting to render #{format.pluralize}/_#{action_name}")
+      render :partial=>"#{format.pluralize}/#{action_name}", :locals=>{:document=>doc}.merge(locals)
+    rescue ActionView::MissingTemplate
+      Rails.logger.debug("rendering default partial catalog/_#{action_name}_partials/default")
+      render :partial=>"catalog/_#{action_name}_partials/default", :locals=>{:document=>doc}.merge(locals)
+    end
+  end
+  
   
 end
