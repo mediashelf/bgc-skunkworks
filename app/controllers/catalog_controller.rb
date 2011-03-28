060d11ef9ccf4354bb6a3ff6534b9179a932cc46
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-28 18:46:38 -0500

    refining implementation of format-specific callbacks

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,10 +1,34 @@
 class CatalogController
   require_dependency 'vendor/plugins/blacklight/app/controllers/catalog_controller.rb'
   
-  include AuctionFilters
+  #
+  # Structures for format-specific setup in before_render filters
+  #
+  include ApplicationHelper # including the helper to give Controller access to document_partial_name method  
+  before_render :format_specific_setup
   
-  # after_filter :setup_auction_info
-  before_render_filters << :setup_auction_info
-  helper :auction
+  # Auction Filters and view helpers
+  include AuctionFilters
+  helper :auction  
+
+  
+  
+  # Run format-specific setup methods
+  # @example
+  #   if the document's format is "auction", it will check for a method called auction_setup
+  def format_specific_setup
+    format = document_partial_name(@document)
+    if self.respond_to?("#{format}_setup".to_sym)
+      send("#{format}_setup".to_sym)
+    else
+      logger.debug "Didn't set up any format-specific info for #{format}.  To enable custom before_render callbacks for this format, define a filter method called #{format}_setup"
+    end
+    # case format
+    # when "auction"
+    #   setup_auction_info
+    # else
+    #   logger.debug "Didn't set up any format-specific info for #{format}"
+    # end
+  end
   
 end
--- vendor/plugins/blacklight/app/helpers/auction_filters.rb
+++ vendor/plugins/blacklight/app/helpers/auction_filters.rb
@@ -1,11 +1,15 @@
 module AuctionFilters
   
+  
+  def auction_setup
+    setup_auction_info
+  end
+  
   def setup_auction_info
     logger.debug "setting up auction info"
     @auction = Auction.load_instance(params[:id])
     params[:per_page] ||= "30"
     @lots = @auction.query_for_lots.paginate(:page=>params[:page], :per_page=>params[:per_page])
-    
   end
   
 end
