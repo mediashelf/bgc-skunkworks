480d20ad01fb4257407b29eac6a20ea6c919fe6b
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-28 18:20:23 -0500

    putting auction_filters into blacklight plugin to avoid cache_classes hell

---
+++ vendor/plugins/blacklight/app/helpers/auction_filters.rb
@@ -0,0 +1,11 @@
+module AuctionFilters
+  
+  def setup_auction_info
+    logger.debug "setting up auction info"
+    @auction = Auction.load_instance(params[:id])
+    params[:per_page] ||= "30"
+    @lots = @auction.query_for_lots.paginate(:page=>params[:page], :per_page=>params[:per_page])
+    
+  end
+  
+end
