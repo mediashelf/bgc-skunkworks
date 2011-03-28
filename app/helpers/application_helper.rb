d0a0d9f006dbcfa364ed5811be9c2470cb22d6b5
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-28 04:11:35 -0500

    minor fixes to render index views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
   def sidebar2_items
     @sidebar2_items ||= []
   end
+  
+  # Overrridden to explicitly reference catalog views
+  # Save function area for search results 'index' view, normally
+  # renders next to title. Includes just 'Folder' by default.
+  def render_index_doc_actions(document, options={})   
+    content_tag("div", :class=>"documentFunctions") do
+      "#{render(:partial => 'catalog/bookmark_control', :locals => {:document=> document}.merge(options))}
+       #{render(:partial => 'catalog/folder_control', :locals => {:document=> document}.merge(options))}"
+    end
+  end
+  
+  
 end
